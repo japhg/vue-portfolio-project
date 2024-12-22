9e4bad02a040f3efe860d7eaf50175ebcaaa0c7a
japhg <jpgomera19@gmail.com>
2024-12-21 22:53:35 -0800

    Added index.js same code in index.ts

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,25 @@
-import { createRouter, createWebHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
-import ProjectView from "../views/ProjectView.vue";
-
-const routes = [
-  { path: "/", component: HomeView },
-  { path: "/about", component: AboutView },
-  { path: "/project", component: ProjectView },
-];
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-});
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue'),
+    },
+    {
+      path: '/project',
+      name: 'project',
+      component: () => import('../views/ProjectView.vue'),
+    }
+  ],
+})
 
-export default router;
+export default router
