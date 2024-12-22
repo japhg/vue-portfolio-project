731a22ad0669fe2534482d0ece19a906e4e9442a
japhg <jpgomera19@gmail.com>
2024-12-21 21:39:09 -0800

    Fixed the issue in Router

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,17 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import AboutView from "../views/AboutView.vue";
+import ProjectView from "../views/ProjectView.vue";
+
+const routes = [
+  { path: "/", component: HomeView },
+  { path: "/about", component: AboutView },
+  { path: "/project", component: ProjectView },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
-    },
-    {
-      path: '/project',
-      name: 'project',
-      component: () => import('../views/ProjectView.vue'),
-    }
-  ],
-})
+  routes,
+});
 
-export default router
+export default router;
