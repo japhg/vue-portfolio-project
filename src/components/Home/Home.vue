b61d5cba78a75e1387c72054d1508b2417dd3fa3
James Philip Gomera <jpgomera19@gmail.com>
2025-02-27 19:13:48 +0800

    Added basic animation. Not Final)

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -3,14 +3,14 @@
     class="w-full max-w-7xl flex justify-center text-center items-start flex-col gap-2"
   >
     <div class="relative w-full mb-0 md:mb-3">
-      <h1 class="text-secondary text-4xl md:text-[83px] leading-snug font-light">
+      <h1 class="text-secondary text-4xl md:text-[83px] leading-snug font-light animate__animated animate__fadeInUp animate__delay-.8s">
         JAMES PHILIP <span class="text-softGray">GOMERA</span>
       </h1>
     </div>
 
     <div>
       <p
-        class="text-secondary text-sm md:text-lg font-light leading-relaxed"
+        class="text-secondary text-sm md:text-lg font-light leading-relaxed animate__animated animate__fadeInUp animate__delay-.8s"
       >
         A
         <strong class="font-bold text-secondary"
@@ -25,7 +25,7 @@
     <div class="mt-6 w-full">
       <RouterLink to="/about" class="">
         <button
-          class="text-secondary border border-secondary px-5 py-3 rounded-full font-light text-sm md:text-base hover:bg-secondary hover:text-white transition-colors"
+          class="text-secondary border border-secondary px-5 py-3 rounded-full font-light text-sm md:text-base hover:bg-secondary hover:text-white transition-colors animate__animated animate__fadeInUp animate__delay-.8s"
         >
           <span class="flex items-center gap-1">
             About more
